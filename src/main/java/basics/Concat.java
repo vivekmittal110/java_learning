57a0e0a358ceca6e5f71342fabeb69f94c177f6a
vivekmittal110 <vivekmittal110230@gmail.com>
2025-12-28 19:12:56 +0530

    Add java basics upto variables

---
+++ src/main/java/basics/Concat.java
@@ -0,0 +1,8 @@
+package basics;
+
+public class Concat {
+    public static void main(String[] args){
+        int x = 20;
+        System.out.print("the value of x is " + x);
+    }
+}
---
+++ src/main/java/basics/variables.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class variables{
+    public static void main(String[] args){
+        int x;
+        x = 309;
+        System.out.println(x+1);
+
+        int y,z;
+        y = 10;
+        z = 30;
+        System.out.println(y+z);
+        System.out.println(y-z);
+        System.out.println(y*z);
+        System.out.println(z/y);
+    }
+}
