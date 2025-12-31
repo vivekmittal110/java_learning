f341a606fa9097c040f7b0961b7e499462af91d6
vivekmittal110 <vivekmittal110230@gmail.com>
2025-12-31 22:06:08 +0530

    basics of methods in java

---
+++ src/main/java/Methods/BasicMethod.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class BasicMethod {
+    public static void main(String[] args) {
+        fun();
+    }
+
+    public static void fun() {
+        System.out.println("function called");
+    }
+}
---
+++ src/main/java/Methods/GreatestThreeMethod.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class GreatestThreeMethod {
+    public static void main(String[] args) {
+        int a = 3332, b = 3132, c = 932;
+//        if((Math.max(a,b))>(Math.max(b,c))){
+//            System.out.println(Math.max(a,b));
+//        }
+//        else{
+//            System.out.println(Math.max(b,c));
+//        }
+        System.out.println(Math.max(a,Math.max(b,c)));
+    }
+}
