5d97d1ebd95eed6ec7ad4f7351023c17dc53c35b
vivekmittal110 <vivekmittal110230@gmail.com>
2026-01-04 21:58:05 +0530

    implement oops in java

--- src/main/java/oops/StudentClass.java
+++ src/main/java/oops/StudentClass.java
@@ -6,10 +6,20 @@ public class StudentClass {
         int roll;
         double percentage;
     }
+    public static void fun(Student s1){
+        s1.name = "new";
+    }
     public static void main(String[] args){
         Student s1 = new Student();
-        s1.name = "vivek";
+        s1.name = "Vivek";
         s1.roll = 97;
-        s1.percentage = 98.6;
+        s1.percentage = 98.8;
+        System.out.println(s1.name);
+        s1.name = "mittal";
+        System.out.println(s1.name);
+        fun(s1);
+        System.out.println(s1.name);
     }
 }
+
+
