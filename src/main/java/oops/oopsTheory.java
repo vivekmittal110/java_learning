53a5e0292a95dd84764d1a7ec691e26cc5459569
vivekmittal110 <vivekmittal110230@gmail.com>
2026-01-05 20:43:41 +0530

    get basics of oops topics

---
+++ src/main/java/oops/oopsTheory.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class oopsTheory {
+    /* User defined data type
+08:06 Classes
+12:55 Objects
+23:13 True or False
+23:55 Array vs Class
+25:19 Revisiting Scanner class
+29:57 Classes inside main
+31:30 Passing class to functions
+41:08 Class in different file
+46:28 Default values of attributes of a class
+49:05 Access modifiers
+59:48 Getters and Setters
+1:07:57 this keyword
+1:12:54 Constructor
+1:27:33 final keyword
+1:31:33 static keyword
+1:43:30 static functions
+1:51:37 Fraction class
+2:12:19 Creating ArrayList*/
+}
