c709ae60a83223646fcf9316ed0c4adcfe88b131
vivekmittal110 <vivekmittal110230@gmail.com>
2026-01-09 21:08:00 +0530

    practice basics of java

---
+++ src/main/java/practiceset2/Pbasic.java
@@ -0,0 +1,12 @@
+package practiceset2;
+class Pbasic{
+    public static void main(String[] args){
+        int num;
+        num = 100;
+        System.out.println("This is num: " + num);
+        num = num *2;
+        System.out.print("The value of num is now ");
+        System.out.println(num);
+        if (num < 201) System.out.println("num is less then 201");
+    }
+}
