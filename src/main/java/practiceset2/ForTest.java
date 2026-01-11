bdedfc70b34596c7e92db64bb93d5525076057c0
vivekmittal110 <vivekmittal110230@gmail.com>
2026-01-11 22:14:05 +0530

    For test

---
+++ src/main/java/practiceset2/ForTest.java
@@ -0,0 +1,9 @@
+package practiceset2;
+class ForTest {
+    public static void main(String[] args){
+        int x;
+        for (x = 0; x < 10; x++){
+            System.out.println(x);
+        }
+    }
+}
