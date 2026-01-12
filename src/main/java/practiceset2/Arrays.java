562c67b255f4500497d6c9701a0ecbf0f5c535ee
vivekmittal110 <vivekmittal110230@gmail.com>
2026-01-12 22:34:35 +0530

    Array revise

---
+++ src/main/java/practiceset2/Arrays.java
@@ -0,0 +1,12 @@
+package practiceset2;
+
+public class Arrays {
+    public static void main(String[] args){
+//        int[] month_days = new int[12];
+//        int month_days[];
+//        month_days = new int[12];
+//        month_days[0] = 8;
+        int month_days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        System.out.println(month_days[0]+month_days[5]);
+    }
+}
